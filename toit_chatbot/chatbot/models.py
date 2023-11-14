1da9a5cfb12aa96cba4bb335db84684d261cc2fd
muhuchah <m.h.chah4@gmail.com>
2023-11-14 07:03:05 +0330

    add password to User model

--- toit_chatbot/chatbot/models.py
+++ toit_chatbot/chatbot/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
     can_make_chatbot = models.BooleanField(default=False)
 
 class Chatbot(models.Model):
