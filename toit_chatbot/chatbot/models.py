c3c0fa42ba025442fc15166370e0af0e4eef77dc
muhuchah <m.h.chah4@gmail.com>
2023-11-14 06:12:50 +0330

    update chatbot models

--- toit_chatbot/chatbot/models.py
+++ toit_chatbot/chatbot/models.py
@@ -1,25 +1,27 @@
 from django.db import models
 
 class User(models.Model):
-    user_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=30)
+    can_make_chatbot = models.BooleanField(default=False)
 
 class Chatbot(models.Model):
-    chatbot_name = models.CharField(max_length=50)
-    chatbot_bio = models.CharField(max_length=300)
+    name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=100)
     # chatbot image
-    chatbot_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Chatbot_data(models.Model):
-    data = models.CharField(max_length=1000)
+    data = models.CharField(max_length=800)
     chatbot = models.ForeignKey(Chatbot, on_delete=models.CASCADE)
 
 class Chat(models.Model):
-    ...
+    chatbot = models.ForeignKey(Chatbot, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Message(models.Model):
-    message_text = models.CharField(max_length=200)
+    text = models.CharField(max_length=100)
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
 
 class Comment(models.Model):
-    comment_text = models.BooleanField()
+    text = models.BooleanField()
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
