951ddd59dcc175cb9ce3d420797df46e4a25adb6
muhuchah <m.h.chah4@gmail.com>
2023-11-14 10:13:55 +0330

    add urls.py in chatbot app, ad chatbot app in main urls.py, add SignOut in chatbot app

--- toit_chatbot/chatbot/views.py
+++ toit_chatbot/chatbot/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def signout(request):
+    messages.success(request, "Logged Out Successfully!!")
+    return redirect('home')
