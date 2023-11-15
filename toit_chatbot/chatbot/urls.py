87eb8d9d284647875b363e5c342537a08fab32c4
muhuchah <m.h.chah4@gmail.com>
2023-11-15 17:27:03 +0330

    add chatbots_list in views, urls, templates

--- toit_chatbot/chatbot/urls.py
+++ toit_chatbot/chatbot/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('chatbots_list/', views.chatbots_list, name='chatbots_list'),
     path('signout/', views.signout, name='signout'),
 ]
--- toit_chatbot/chatbot/views.py
+++ toit_chatbot/chatbot/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from chatbot.models import Chatbot
+
+def chatbots_list(request):
+    chatbots = Chatbot.objects.all()
+    
+    context = {
+        "chatbots" : chatbots
+    }
+
+    return render(request, "chatbot/chatbots_list.html", context)
 
 def signout(request):
     messages.success(request, "Logged Out Successfully!!")
