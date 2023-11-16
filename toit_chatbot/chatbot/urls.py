2ce50e86190738ee0160b6a5fba65c30bb40dbe9
muhuchah <m.h.chah4@gmail.com>
2023-11-16 06:23:11 +0330

    add chat_detail in views, urls, templates

--- toit_chatbot/chatbot/urls.py
+++ toit_chatbot/chatbot/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('chatbots_list/', views.chatbots_list, name='chatbots_list'),
     path('chat_history/<int:user_id>/<int:chatbot_id>/', views.chat_history, name='chat_history'),
+    path('chat_detail/<int:chat_id>/', views.chat_detail, name='chat_detail'),
     path('signout/', views.signout, name='signout'),
 ]
--- toit_chatbot/chatbot/views.py
+++ toit_chatbot/chatbot/views.py
@@ -16,14 +16,24 @@ def chat_history(request, user_id, chatbot_id):
     user = get_object_or_404(User, id=user_id)
     chatbot = get_object_or_404(Chatbot, id=chatbot_id)
     
-    chats_titles = Chat.objects.filter(user=user, chatbot=chatbot).values_list('title', flat=True)
+    chats_info = Chat.objects.filter(user=user, chatbot=chatbot).values_list('title', 'id')
+    print("-------------------------------")
+    print(chats_info[0][0], chats_info[0][1])
+    print("-------------------------------")
 
     context = {
         'chatbot_name': chatbot.name,
-        'chats_titles': chats_titles
+        'chats_info': chats_info
     }
 
     return render(request, "chatbot/chat_history.html", context)
+
+
+def chat_detail(request, chat_id):
+    chat = get_object_or_404(Chat, id=chat_id)
+
+    return render(request, 'chatbot/chat_detail.html', {'chat': chat})
+
 
 def signout(request):
     messages.success(request, "Logged Out Successfully!!")
