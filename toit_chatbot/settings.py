04f49f346c975ca100095e4ea0eb382833cb9f5a
muhuchah <m.h.chah4@gmail.com>
2023-11-22 17:13:27 +0330

    add CSRF_TRUSTED_ORIGINS in settings.py

--- toit_chatbot/settings.py
+++ toit_chatbot/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'toit_chatbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -87,7 +88,6 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -137,4 +137,8 @@ INTERNAL_IPS = [
     # ...
     "127.0.0.1",
     # ...
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://toitchatbot.darkube.app'
 ]
